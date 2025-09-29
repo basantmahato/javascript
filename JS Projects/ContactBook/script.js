78a2ab3bf988ba9237921523b1915d16362f4a93
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-09-29 12:46:03 +0530

    Day5

--- JS Projects/ContactBook/script.js
+++ JS Projects/ContactBook/script.js
@@ -2,23 +2,20 @@ const nameInput = document.getElementById("name");
 const phoneInput = document.getElementById("phone");
 const emailInput = document.getElementById("email");
 
-const addContactBtn = document.getElementById("addContactBtn")
-const contactList = document.getElementById("contactList")
-const searchData = document.getElementById("search")
+const addContactBtn = document.getElementById("addContactBtn");
+const contactList = document.getElementById("contactList");
+const searchData = document.getElementById("search");
 
-
-let phonebook = []
+let phonebook = [];
 
 const render = (dataToRender = phonebook) => {
-    contactList.innerHTML = ""
-    dataToRender.forEach(function(contact){
-        const li = document.createElement("li")
-        li.innerHTML = `Name : ${contact.name} Phone: ${contact.phone} Email: ${contact.email}`
-        contactList.appendChild(li)
-    })
-
-}
-
+  contactList.innerHTML = "";
+  dataToRender.forEach(function (contact) {
+    const li = document.createElement("li");
+    li.innerHTML = `Name : ${contact.name} Phone: ${contact.phone} Email: ${contact.email}`;
+    contactList.appendChild(li);
+  });
+};
 
 // const renderFiltered = (filtered) => {
 //     contactList.innerHTML = ""
@@ -31,61 +28,53 @@ const render = (dataToRender = phonebook) => {
 
 // }
 
-addContactBtn.addEventListener('click', (e)=>{
+addContactBtn.addEventListener("click", (e) => {
+  const contactForm = document.getElementById("contactForm");
 
-    const contactForm = document.getElementById("contactForm")
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
 
-    contactForm.addEventListener('submit', (e)=>{
-        e.preventDefault()
-    })
+  const name = nameInput.value;
+  const phone = phoneInput.value;
+  const email = emailInput.value;
 
+  console.log(name, phone, email);
 
-    const name = nameInput.value;
-    const phone = phoneInput.value;
-    const email = emailInput.value;
+  const nameKey = "name";
+  const phoneKey = "phone";
+  const emailKey = "email";
 
-    console.log(name , phone , email)
+  let obj = {};
 
-    const nameKey = "name"
-    const phoneKey = "phone"
-    const emailKey = "email"
+  obj[nameKey] = name;
+  obj[phoneKey] = phone;
+  obj[emailKey] = email;
 
-    let obj = {}
+  phonebook.push(obj);
 
-    obj[nameKey] = name
-    obj[phoneKey] = phone
-    obj[emailKey] = email
+  console.log(phonebook);
 
-    phonebook.push(obj)
+  render();
 
-    console.log(phonebook)
-
-    render()
-
-   
-        nameInput.value = "";
-        phoneInput.value = "";
-        emailInput.value = "";
-    
-
-    
-
-})
-
+  nameInput.value = "";
+  phoneInput.value = "";
+  emailInput.value = "";
+});
 
 function searchContacts(query) {
-    console.log(query + "query")
-  const filtered = phonebook.filter(phonebook =>
-    phonebook.name.toLowerCase().includes(query.toLowerCase())
+  console.log(query + "query");
+  const filtered = phonebook.filter((contact) =>
+    contact.name.toLowerCase().includes(query.toLowerCase())
   );
 
-  console.log(filtered + "filtered")
+  console.log(filtered + "filtered");
   render(filtered);
 }
 
-searchData.addEventListener('input', e => {
-    e.preventDefault()
-    const searchValue = e.target.value
-    console.log(searchValue)
-    searchContacts(searchValue);
+searchData.addEventListener("input", (e) => {
+  e.preventDefault();
+  const searchValue = e.target.value;
+  console.log(searchValue);
+  searchContacts(searchValue);
 });
